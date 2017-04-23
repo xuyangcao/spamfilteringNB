dbf8f3d19b629756af425fbe45f840fde45e00ff
xuyangcao <16120002@bjtu.edu.cn>
2017-04-23 19:53:42 +0800

    final edition

--- Mybayes.py
+++ Mybayes.py
@@ -17,6 +17,8 @@ def createVocabList(docList):
     for document in docList:
         VocabSet = VocabSet|set(document)
     return list(VocabSet)
+    
+# remove numbers from vocabulary of list
 def Nonum(VocabList):
     NonumList = []
     for i in range(len(VocabList)):
@@ -65,13 +67,54 @@ def trainNB(trainMatrix,trainClass):
     return p0Vect,plVect,pAbusive
 
 
-def classifyNB(wordVector,P0v,plV,pSpam):
+def classifyNB(wordVector,P0v,plV,pSpam, th):
     pl = sum(wordVector * plV) + np.log(pSpam)
     P0 = sum(wordVector * P0v) + np.log(1.0 - pSpam)
-    if pl > P0:
+    print 'p1: '+str(pl)
+    print 'p0: '+str(P0)
+    print 'p1 - p0: '+ str(pl - P0)+'\n'
+    if pl - P0 > th:
         return 1
     else:
         return 0
+    
+    
+def classifrisk(wordVector,P0v,plV,pSpam,th):#基于最小风险分类
+    p1 = sum(wordVector * plV)
+    p0 = sum(wordVector * P0v)
+    a01 = 1;a10 = 2
+    th = np.log(a01*pSpam)-np.log(a10*(1-pSpam))
+    if p1/float(p0)>th:
+        return 0;
+    else:
+        return 1;
+    
+    
+def count_tfidf(classList,NonumList,docList,p0v,p1v):
+    spamnum = sum(classList)
+    hamnum = len(classList)- spamnum
+    p0idf =[];p1idf = []
+    for i in range(len(NonumList)):
+        num = 0
+        for j in range(len(classList)):
+            if classList[j] == 1:#如果垃圾邮件
+                if NonumList[i] in docList[j]:
+                    num = num +1
+        idf1 = np.log(spamnum/float(1+num))
+        p1idf.append(idf1)
+    p1idf = np.array(p1idf)
+    for i in range(len(NonumList)):
+        num = 0
+        for j in range(len(classList)):
+            if classList[j] ==0:#如果不是垃圾邮件
+                if NonumList[i] in docList[j]:
+                    num =num+1
+        idf0 = np.log(hamnum/float(1 + num))
+        p0idf.append(idf0)
+    p0idf = np.array(p0idf)
+    p1tfidf = p1v*p1idf
+    p0tfidf = p0v*p0idf
+    return p1idf,p0idf,p1tfidf,p0tfidf
 
 def createFinalList(p0v,p1v,NonumList):
     pp = [];finalList = []
@@ -96,8 +139,50 @@ def createFinalList(p0v,p1v,NonumList):
             fp1v.append(pp1v[i])
     return finalList,fp1v,fp0v
 
+def createFinalList2(p0idf,p1idf,NonumList,p0v,p1v):
+    pp = [];finalList2 = []
+    pp0v = p0idf.tolist()
+    pp1v = p1idf.tolist()
+    pidf = p1idf- p0idf
+    pp = pidf.tolist()
+    #maxv = max(pp)
+    #minv = min(pp)
+    #th = (maxv-minv)*0.8#(maxv - minv)
+    fp0v2 =[];fp1v2 = []
+    for i in range(len(pp)):
+        if pp[i]<2.5 and pp[i]>-2.5:
+            pp0v[i] = 0
+            pp1v[i] = 0
+        else:
+            finalList2.append(NonumList[i])
+            fp0v2.append(p0v[i])
+            fp1v2.append(p1v[i])
+            
+    return finalList2,fp1v2,fp0v2
 
-    
+
+def countROC(tsetresult,testLabel):
+    tp = 0;fp =0;tn =0;fn =0
+    for i in range(len(testLabel)):
+        if testLabel[i] == 1:
+            if tsetresult[i] != testLabel[i]:#À¬»øÓÊ¼þ´í·ÖÀà
+                fn = fn+1
+            else:
+                tp = tp+1
+        if testLabel[i]== 0:
+            if tsetresult[i] !=testLabel[i]:
+                fp =fp+1
+            else:
+                tn = tn+1
+    TPF = tp/float(tp+fn)
+    TNF = tn/float(fp+tn)
+    return TNF,TPF
+
+def getAUC(rocx, rocy):
+    sum = 0
+    for i in range(len(rocx) - 1):
+        sum += (rocy[i] + rocy[i+1]) * (rocx[i+1] - rocx[i]) / 2
+    return sum          
             
             
             
--- main.py
+++ main.py
@@ -11,20 +11,23 @@ import Mybayes as my
 #sys.setdefaultencoding('utf8')   
 import numpy as np
 import time
+import pylab as pl
 
 errorrate = []
+auc = []
 
 s = 'spm'
 pth = 'C:/Users/new_xuyangcao/Desktop/spamfilteringNB/lingspam_public_s/lemm/part' 
 #全部数据的读取
 print pth
 
-for i in range(1):
+for i in range(10):
     filename = [];docList = [];fullText = [];classList = [];testLabel = []; testdocList = [];trainMat = []
+    rocx = [0];rocy = [0]
+    temp_auc = 0
     
     print 'the ' + str(i) + 'th fold...' 
-    print '    set up train Mat...'
-    start = time.time()
+    print '    read data from folder...'
     for j in range(10):
         if j != i:
             jj = str(j+1)
@@ -49,9 +52,13 @@ for i in range(1):
                 testLabel.append(r)
                 testdoc = my.textParse(open(pthtest+'\\'+testfiles[tnum]).read())
                 testdocList.append(testdoc)
-  
+                
+    print '    creat vocabulary list...'    
+    start = time.time()        
     VocabList = my.createVocabList(docList)#不重复词列表spam和ham
     NonumList = my.Nonum(VocabList)
+    print '    creat vocabulary list ' + str(time.time() - start) +'s'
+    
     for trainnum in range(len(docList)):
         print '        the '+str(trainnum)+'th doc in '+str(len(docList)) +' docs'
         trainMat.append(my.setOfWords2Vec(NonumList,docList[trainnum]))#利用字典对训练集编码
@@ -68,19 +75,84 @@ for i in range(1):
     print '    testing...'
     strat = time.time()
     errorcount = 0#训练完成设置错去率为0
+    
     #在测试前对List进行化简根据likehood之差进行
-    FinalList , fp1v , fp0v = my.createFinalList(p0v,p1v,NonumList)
-    #开始测试
-    #对测试邮件编码
-    for testnum in range(len(testdocList)):
-        testMat = my.setOfWords2Vec(NonumList,testdocList[testnum])
-
-        re = my.classifyNB(np.array(testMat),p0v,p1v,pspam)
-        if re != testLabel[testnum]:
-            errorcount += 1
-    print '    the error rate: ' + ii + 'is:',str(float(errorcount)/len(testdocList))#得到错误率
-
+#    FinalList , fp1v , fp0v = my.createFinalList(p0v,p1v,NonumList)
+#    file1 = open('List_tf.txt','a+')
+#    file1.write('第'+str(i)+'折提取到的词表:'+str(FinalList)+'\n');  
+#    file1.close()
+    
+    
+#    print 'Final list over!'
+#    test using idf
+    p1idf, p0idf, p1tfidf, p0tfidf = my.count_tfidf(classList,NonumList,docList,p0v,p1v)
+    idf_wordList, fp1v2,fp0v2 = my.createFinalList2(p0idf,p1idf,NonumList,p0v,p1v)
+    file_idf = open('idf_feature_words.txt','a+')
+    file_idf.write('第'+str(i)+'折提取到的词表:'+str(idf_wordList)+'\n');  
+    file_idf.close()
+    
+#    开始测试
+#    对测试邮件编码
+    strat = time.time()
+    for th in range(-100,110, 10):
+        testresult = []      
+        for testnum in range(len(testdocList)):
+            testMat = my.setOfWords2Vec(idf_wordList,testdocList[testnum])
+            re = my.classifyNB(np.array(testMat),fp0v2,fp1v2,pspam, th)
+            testresult.append(re)
+            if re != testLabel[testnum] and th == 0:
+                errorcount += 1
+        TPF,TNF = my.countROC(testresult,testLabel)
+        rocx.append(1-TNF)
+        rocy.append(TPF)     
+    rocx.append(1)
+    rocy.append(1)
+    temp_auc = my.getAUC(rocx, rocy)
+    
+    pl.title('ROC curve of original feature vector')
+    pl.xlabel("FPR")
+    pl.ylabel("TPR")
+    pl.plot(rocx, rocy, '*-')# use pylab to plot x and y
+    pl.show()# show the plot on the screen    
+    pl.savefig('%d.png' % i)
+    print '    the error rate: ' + ii + ' is:',str(float(errorcount)/len(testdocList))#得到错误率
+    print '    the AUC: ', temp_auc
+    
+    #save roc datas
+    file_roc = open('roc_curve_data.txt', 'a+')
+    file_roc.write(ii + 'fold roc curve data:'+str(rocx)+'\n\n'+str(rocy)+'\n\n\n')
+    file_roc.close
+    
+    auc.append(temp_auc)
     errorrate.append(float(errorcount)/len(testdocList))
     print '    testing time: ' +str(time.time()-strat) + 's'
 result = sum(errorrate)/10
+auc_average = sum(auc) / len(auc)
 print 'the average error rate is:',result
+print 'the average auc is :', auc_average
+
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
